2b7a57a1dc39e166285fb6415e7baad16dbb8c18
Raghu K <raghuk.vit@gmail.com>
2026-03-20 08:18:43 +0530

    WMP scripts

---
+++ main.tf
@@ -0,0 +1,32 @@
+resource "aws_instance" "instance" {
+  for_each = var.components
+  ami = "ami-0220d79f3f480ecf5"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-09b871cf63e2bb80b"]
+
+  tags = {
+    Name = var.components[each.key]
+  }
+}
+
+resource "aws_route53_record" "dns" {
+  for_each = var.components
+  zone_id = "Z08243203OP3MKK8XQJHA"
+  name    = "${var.components[each.key]}-dev"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.instance[each.key].private_ip]
+}
+
+variable "components" {
+  default = {
+    frontend = ""
+    postgresql = ""
+    auth-service = ""
+    analytics-service = ""
+    portfolio-service = ""
+  }
+}
+
+
+
