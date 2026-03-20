3d1bfb8b5684bbe0e05d35f73d5d958f5c2cabc3
Raghu K <raghuk.vit@gmail.com>
2026-03-20 08:33:26 +0530

    WMP scripts

--- main.tf
+++ main.tf
@@ -5,18 +5,18 @@ resource "aws_instance" "instance" {
   vpc_security_group_ids = ["sg-09b871cf63e2bb80b"]
 
   tags = {
-    Name = var.components[each.key]
+    Name = each.key
   }
 }
 
-# resource "aws_route53_record" "dns" {
-#   for_each = var.components
-#   zone_id = "Z08243203OP3MKK8XQJHA"
-#   name    = "${var.components[each.key]}-dev"
-#   type    = "A"
-#   ttl     = 30
-#   records = [aws_instance.instance[each.key].private_ip]
-# }
+resource "aws_route53_record" "dns" {
+  for_each = var.components
+  zone_id = "Z08243203OP3MKK8XQJHA"
+  name    = "${each.key}-dev"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.instance[each.key].private_ip]
+}
 
 variable "components" {
   default = {
